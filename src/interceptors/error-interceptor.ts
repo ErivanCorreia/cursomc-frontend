c52bec541773a62a37aca489640370d55aba9383
Erivan <erivann47@gmail.com>
2019-02-11 12:33:26 -0300

    Tratamento para erros 401 e demais erros

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -8,11 +8,14 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
     
-    constructor(public storage: StorageService) {
+    constructor(
+        public storage: StorageService, 
+        public alertController: AlertController) {
     }
     
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
@@ -32,9 +35,16 @@ export class ErrorInterceptor implements HttpInterceptor{
             console.log(errorObj);
 
             switch(errorObj.status) {
+                case 401:
+                   this.handle401();
+                break;
+    
                 case 403: 
                     this.handle403();
                 break;
+
+                default:
+                    this.handleDefaultError(errorObj);
             }
 
             
@@ -42,8 +52,32 @@ export class ErrorInterceptor implements HttpInterceptor{
         }) as any;
     }
 
+    handle401() {
+        let alert = this.alertController.create({
+            title: 'Algo de errado não está certo -_- !!',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'Ok'}
+            ]
+        });
+        alert.present();
+    }
+   
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handleDefaultError(errorobj) {
+        let alert = this.alertController.create({
+            title: ' ' + errorobj.error,
+            message: errorobj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'Ok'}
+            ]
+        });
+        alert.present();
     }
 }
 
